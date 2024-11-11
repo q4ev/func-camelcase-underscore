0b92fb78b679ba7247cb428ac47b3efa34492530
a.chernov <a.chernov@vozovoz.ru>
2024-11-11 14:57:30 +0300

    init

---
+++ src/func.php
@@ -0,0 +1,22 @@
+<?php
+
+class func
+{
+	public static function camelcase2underscore ($string)
+	{
+		return strtoupper($string) === $string
+			// if input $string all caps
+			? $string
+			// otherwise, replace caps with "underscore+lowercase one"
+			: strtolower(preg_replace('/(?<!^)([A-Z]|[0-9])/', '_$0', $string));
+	}
+
+	public static function underscore2camelcase ($string, $ucfirst = false)
+	{
+		$result = str_replace('_', '', ucwords($string, '_'));
+		if (!$ucfirst)
+			$result = lcfirst($result);
+
+		return $result;
+	}
+}
